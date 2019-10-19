2266094a3bc934f4c24255a1985e1842fba9d693
Alfred Lieth Årøe <alfred.aroe97@gmail.com>
2019-10-19 11:42:51 +0200

    Improved gausian blur, speed and correctness

--- gausian_blur.py
+++ gausian_blur.py
@@ -1,36 +1,48 @@
 import numpy as np 
 from PIL import Image
 
+def gausian_blur(im, kernel):
+	''' 
+	A function that convolves im with kernel. The edges outside the image is handled as 0
+	
+	Args:
+		im: np.array of shape [H, W, 3]
+		kernel: np.array of shape [K, K]
+	
+	Returns:
+		np.array of shape [H, W, 3]
+	'''
+
+	padding = int((kernel.shape[0]-1)/2)
+
+	# add a border of zeros to make life simpler inside the for-loops
+	# pads im with zero in top, bottom, left, right, but not in color
+	pad_im = np.pad(im, ((padding,padding), (padding,padding), (0,0)), 'constant')
+
+	blurred = np.zeros(im.shape)
+
+	for row in range(padding, im.shape[0] + padding): # rows
+		for col in range(padding, im.shape[1] + padding): # columns
+			for color in range(im.shape[2]): # colors
+				# pick out a part of the image
+				part = pad_im[row-padding:row+padding+1, col-padding:col+padding+1, color]
+
+				value = int(round(sum(sum(kernel*part))))
+
+				# setting the value
+				blurred[row-padding, col-padding, color] = value
+
+	return blurred
+
 image_name = 'image_0'
 picture = Image.open('images/original/' + image_name + '.jpg')
 array = np.array(picture)
 
-blurred = np.zeros(array.shape)
-
-print(array.shape) # (rows, columns, colors)
-
 filter_size = 11 # must be odd
 filter_value = 1 
+gaus = np.full((filter_size, filter_size), filter_value) / (filter_size*filter_size)
 
-# the range to loop over for our filter
-start = int(-((filter_size - 1) / 2))	
-end   = int(((filter_size - 1) / 2) + 1)
-
-gaus = np.full((filter_size, filter_size), filter_value)
-
-for row in range(array.shape[0]): # rows
-	for col in range(array.shape[1]): # columns
-		for color in range(array.shape[2]): # colors
-			value = 0 # the total value of pixels
-			num_values = 0 # will most times be filter_size^2, but not on the edges
-			for r in range(start, end):
-				for c in range(start, end):
-					if row + r > 0 and row + r < array.shape[0] and col + c > 0 and col + c < array.shape[1]:
-						# we are inside the bounds
-						value += gaus[r][c]*array[row+r][col+c][color]
-						num_values += 1
-
-			blurred[row][col][color] = value/num_values # mean of the values
+blurred = gausian_blur(array, gaus)
 
 blurred_image = Image.fromarray(blurred.astype('uint8'))
 blurred_image.show()
